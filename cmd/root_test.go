3ff53aa7dda261067c3dfd99dc6c3aa1e647c7bb
Christopher Lamm <christopher.lamm@companycam.com>
2023-08-18 22:35:18 -0400

    added test for Execute

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -6,23 +6,39 @@ import (
 	"testing"
 )
 
-func Test_versionCMD(t *testing.T) {
+func Test_Root(t *testing.T) {
 	old := os.Stdout
 	r, w, _ := os.Pipe()
 	os.Stdout = w
 
-	versionCmd.Execute()
+	t.Run("test Execute", func(t *testing.T) {
+		Execute()
+
+		out := make([]byte, 100)
+		n, _ := r.Read(out)
+
+		expected := "hcli v0"
+		actual := strings.TrimSpace(string(out[:n]))
+
+		if actual != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, actual)
+		}
+	})
+
+	t.Run("test version", func(t *testing.T) {
+		versionCmd.Execute()
+
+		out := make([]byte, 100)
+		n, _ := r.Read(out)
+
+		expected := "hcli v0"
+		actual := strings.TrimSpace(string(out[:n]))
+
+		if actual != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, actual)
+		}
+	})
 
 	w.Close()
 	os.Stdout = old
-
-	out := make([]byte, 100)
-	n, _ := r.Read(out)
-
-	expected := "hcli v0"
-	actual := strings.TrimSpace(string(out[:n]))
-
-	if actual != expected {
-		t.Errorf("expected '%s' but got '%s'", expected, actual)
-	}
 }
